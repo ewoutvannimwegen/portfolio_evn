405560beb590440b4c57147b730f2d57f80bb2a7
Ewout van Nimwegen <ewoutvannimwegen@gmail.com>
2020-08-03 10:19:46 +0200

    Updates

--- js/script.js
+++ js/script.js
@@ -18,19 +18,19 @@ for (var i = 0; themeDots.length > i; i++) {
 
 function setTheme(mode) {
   if (mode == 'light') {
-    document.getElementById('theme-style').href = '/dist/css/default.css';
+    document.getElementById('theme-style').href = 'css/default.css';
   }
 
   if (mode == 'blue') {
-    document.getElementById('theme-style').href = '/dist/css/blue.css';
+    document.getElementById('theme-style').href = 'css/blue.css';
   }
 
   if (mode == 'green') {
-    document.getElementById('theme-style').href = '/dist/css/green.css';
+    document.getElementById('theme-style').href = 'css/green.css';
   }
 
   if (mode == 'purple') {
-    document.getElementById('theme-style').href = '/dist/css/purple.css';
+    document.getElementById('theme-style').href = 'css/purple.css';
   }
 
   localStorage.setItem('theme', mode);
